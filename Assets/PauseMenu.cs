0988b4b58b58a9810979663c344f7483d4c02964
gtuts <99958293+gtuts@users.noreply.github.com>
2022-06-10 14:24:01 +0300

    PauseMenu ready
    Pausi menüü valmis

--- Assets/PauseMenu.cs
+++ Assets/PauseMenu.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour {
 
@@ -18,7 +19,7 @@ public class PauseMenu : MonoBehaviour {
         }
     }
 
-    void Resume () {
+    public void Resume () {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
         GameIsPaused = false;
@@ -27,6 +28,14 @@ public class PauseMenu : MonoBehaviour {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
         GameIsPaused = true;
+    }
+    public void LoadMenu() {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MainMenu");
+    }
+    public void QuitGame() {
+        Debug.Log("Sulgen mängu...");
+        Application.Quit();
     }
 
 }
