3906e9e3b53cb570d9db348db6caa1973f532443
gtuts <99958293+gtuts@users.noreply.github.com>
2022-06-10 16:06:30 +0300

    WeaponMenu
    Relva menüü, vaja saada scened korda ja siis saab weapon menüü liita. Peale seda saab ka määrata mis relvad on valikus.

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -6,7 +6,8 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void PlayGame() {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("WeaponMenu");
     }
 
     public void QuitGame() {
