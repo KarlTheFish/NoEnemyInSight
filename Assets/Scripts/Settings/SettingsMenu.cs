0588b569cb01ad8c238d108062ec04abeabae443
Kevin Rosenberg <90192448+KevinRosenbrg@users.noreply.github.com>
2022-06-14 15:13:57 +0300

    Localization sättete muutmine poolelii
    keeli saab läbi unity vahetada, aga mitte mängus sees

--- Assets/Scripts/Settings/SettingsMenu.cs
+++ Assets/Scripts/Settings/SettingsMenu.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Localization;
 
 public class SettingsMenu : MonoBehaviour
 {
+    // LocalizationSettings settings = LocalizationSettings.Instance;
+    
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {   
-            print("space key was pressed");
-        }      
+        // Debug.Log(LocalizationSettings.SelectedLocale);    
     }
 }
