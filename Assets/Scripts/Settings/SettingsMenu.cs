8261330429f72d8809cead9b2cc6c7070d641385
Kevin Rosenberg <90192448+KevinRosenbrg@users.noreply.github.com>
2022-06-15 11:13:27 +0300

    Keele sätted lisatud
    Sättete menüüs olemas keelte menüü, kus saab keeli vahetada

--- Assets/Scripts/Settings/SettingsMenu.cs
+++ Assets/Scripts/Settings/SettingsMenu.cs
@@ -1,14 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Localization;
+using UnityEngine.Localization.Settings;
+// using UnityEngine.Localization;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
-    // LocalizationSettings settings = LocalizationSettings.Instance;
+    public void SetSelectedLocale(int index)
+    {
+        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[index];
+    }
     
+    /* 
     void Update()
     {
-        // Debug.Log(LocalizationSettings.SelectedLocale);    
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SetSelectedLocale(0); // set to eng
+        }    
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            SetSelectedLocale(1); // set to est
+        }  
+    }
+    */
+    
+    public void SetEnglish(){
+        SetSelectedLocale(0);
+    }
+
+    public void SetEstonian(){
+        SetSelectedLocale(1);
     }
 }
