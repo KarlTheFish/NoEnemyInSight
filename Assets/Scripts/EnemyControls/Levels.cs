d01651c537d15572219a265bf8a99a1f5b8d5df6
Karl Piiber <90192557+KarlP02@users.noreply.github.com>
2022-06-17 13:51:35 +0300

    rohkem leveleid

--- Assets/Scripts/EnemyControls/Levels.cs
+++ Assets/Scripts/EnemyControls/Levels.cs
@@ -1,9 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Levels : MonoBehaviour
 {
+    public PlayerStats playerStats;
+
     public int level1;
-    public int level2;
+    public static int levelNumber = 1;
+    public static int nextLevel = 1;
+    public static int lastScreen = 0;
+
+    private void Update()
+    {
+        if (playerStats.playerHealth == 0)
+        {
+            SceneManager.LoadScene(4);
+        }
+        if (level1 == 0)
+        {
+            SceneManager.LoadScene(5);
+            if (lastScreen == 2)
+            {
+                SceneManager.LoadScene(6);
+            }
+        }
+        Debug.Log(lastScreen);
+    }
 }
